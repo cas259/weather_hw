78fc3bc9946f9ffc65360d8a062d0f6ae961c15d
Candice Stewart <candice-stewart@kellogg.northwestern.edu>
2016-02-03 06:21:04 -0600

    Current & 3-day Forecast

--- weather.js
+++ weather.js
@@ -2,12 +2,20 @@ $(function() {
   var handleWeatherResponse = function(data) {
     console.log(data);
 
-    // Put your code here to change the "markup" variable. 
+    // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
-    
-    var markup = "The weather report... " + "will be here when I finish my homework.";
-    
-    
+
+    var markup =
+    "<h1>Chicago Weather</h1>" +
+
+   "<h3>Current Conditions</h3>" +
+   "<ul><li>It is currently " + data.currently.summary +
+
+    "<h3>3-Day Forecast</h3>" +
+      "<ul><li>Tomorrow, the high will be " + data.daily.data[1].temperatureMax + " degrees." +
+      "<li>The next day, the high will be " + data.daily.data[2].temperatureMax + " degrees." +
+      "<li>Three days from now, the high will be " + data.daily.data[3].temperatureMax + " degrees.</ul>"
+
     // End of your code
 
     $('.weather-report').html(markup);
